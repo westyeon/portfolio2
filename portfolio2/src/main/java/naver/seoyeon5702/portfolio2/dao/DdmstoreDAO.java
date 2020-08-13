1bb449cd452db02e79d8f76d56a201065a490af8
a30418 <a30418@218.38.137.28>
2020-08-13 10:33:17 +0900

    2020-08-12 DaoTest까지 마

--- portfolio2/src/main/java/naver/seoyeon5702/portfolio2/dao/DdmstoreDAO.java
+++ portfolio2/src/main/java/naver/seoyeon5702/portfolio2/dao/DdmstoreDAO.java
@@ -14,6 +14,33 @@ public class DdmstoreDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	
+	//searchtype과 keyword를 이용해서 데이터 개수 찾아오는 메소드
+	public int count (Map<String, Object> map) {
+		return sqlSession.selectOne("ddmstore.count", map);
+	}
+	
+	//searchtype과 keyword 와 start 와 end를 데이터 목록을 찾아오는 메소드
+	public List<Ddmstore> list (Map<String, Object> map){
+		return sqlSession.selectList("ddmstore.list", map);
+	}
+	
+	//store_number에 해당하는 데이터 1개를 메소드
+	public Ddmstore detail(String store_number) {
+		return sqlSession.selectOne("ddmstore.detail", store_number);
+	}
+	
+	//가장 큰 store_number를 찾아오는 메소드
+		public String maxnumber() {
+			return sqlSession.selectOne("ddmstore.maxnumber");
+		}
+	
+	//데이터를 삽입하는 메소드
+	public int insert(Ddmstore ddmstore) {
+		return sqlSession.insert("ddmstore.insert", ddmstore);
+	}
+	//데이터를 삽입하는메소
+	/*
 	//데이터 번호를 받아서 번호에 해당하는 데이터를 가져오는 메소드
 	public List<Ddmstore> list(Map map){
 		return sqlSession.selectList("ddmstore.list",map);
@@ -21,6 +48,6 @@ public class DdmstoreDAO {
 	//store_number 를 받아서 1개의데이터 가져오는 메소드
 	public Ddmstore detail (String store_number){
 		return sqlSession.selectOne("ddmstore.detail",store_number);
-	}
+	}*/
 
 }
